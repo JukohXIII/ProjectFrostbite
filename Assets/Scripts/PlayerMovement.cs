0fd632a5ed46fca0cbbabc252c31b0e0531ea393
Jérémy Vanin <jeremyvanin@proton.me>
2024-01-19 19:37:22 +0100

    Made movements more sharp + added animations to movements and attacking

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-
     private float speed = 8f;
 
-    private float jumpingPower = 20f;
+    private float jumpingPower = 30f;
 
     private bool isFacingRight = true;
     private bool isMoving = false;
@@ -20,15 +20,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
 
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Animator animator;
     
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        if (horizontal)
-        {
-            isMoving = true;
-        }
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             isJumping = true;
@@ -39,9 +37,28 @@ public class PlayerMovement : MonoBehaviour
             isFalling = true;
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y * 0.5f);
         }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            goDown();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetBool("LightAttack",true);
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            animator.SetBool("LightAttack",false);
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            animator.SetBool("HeavyAttack",true);
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            animator.SetBool("HeavyAttack",false);
+        }
         Flip();
     }
-
     private void FixedUpdate()
     {
         rigidBody.velocity = new Vector2(horizontal * speed, rigidBody.velocity.y);
@@ -59,6 +76,13 @@ public class PlayerMovement : MonoBehaviour
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
+        }
+    }
+    private void goDown()
+    {
+        if (IsGrounded() == false)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, -10);
         }
     }
 }
