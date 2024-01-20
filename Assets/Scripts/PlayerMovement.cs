ad969928f7eee9768470f42ff877fa309ddb5f09
Jérémy Vanin <jeremyvanin@proton.me>
2024-01-20 02:01:17 +0100

    Managed to make the fall slightly faster than the jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,14 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
-
-    private float jumpingPower = 30f;
-
+    private float jumpingPower = 15f;
     private bool isFacingRight = true;
     private bool isMoving = false;
     private bool isJumping = false;
     private bool isFalling = false;
-
+    
     [SerializeField] private Rigidbody2D rigidBody;
-
     [SerializeField] private Transform groundCheck;
-
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Animator animator;
     
@@ -27,16 +23,38 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        CheckJumping();
+        CheckKeys();
+        ChangeSpriteDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = new Vector2(horizontal * speed, rigidBody.velocity.y);
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+    private void ChangeSpriteDirection()
+    {
+        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
-            isJumping = true;
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpingPower);
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
         }
-        if (Input.GetButtonDown("Jump") && rigidBody.velocity.y > 0f)
+    }
+    private void goDown()
+    {
+        if (IsGrounded() == false)
         {
-            isFalling = true;
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y * 0.5f);
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, -10);
         }
+    }
+
+    private void CheckKeys(){
         if (Input.GetKeyDown(KeyCode.S))
         {
             goDown();
@@ -57,32 +75,21 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetBool("HeavyAttack",false);
         }
-        Flip();
     }
-    private void FixedUpdate()
-    {
-        rigidBody.velocity = new Vector2(horizontal * speed, rigidBody.velocity.y);
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
+    private void CheckJumping(){
+        if(Input.GetButtonDown("Jump") && IsGrounded()){
+            isJumping = true;
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpingPower);
         }
-    }
-    private void goDown()
-    {
-        if (IsGrounded() == false)
+
+        if (Input.GetButtonDown("Jump") && rigidBody.velocity.y > 0f)
         {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, -10);
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y);
+        }
+
+        if (rigidBody.velocity.y < 0f)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 1.001f * rigidBody.velocity.y);
         }
     }
 }
