2cbbc591faa4c058581babab0ccf4c33b56162c9
Jérémy Vanin <jeremyvanin@proton.me>
2024-04-13 20:33:52 +0200

    Walking animtion & taunt animation added

--- Assets/Scripts/BossBehavior.cs
+++ Assets/Scripts/BossBehavior.cs
@@ -7,31 +7,40 @@ public class BossBehavior : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private Animator animator;
 
-
-    private float speed = 5f;
-    private bool isMoving = false;
+    [SerializeField] private float speed = 5f;
     private SpriteRenderer bossSpriteRenderer;
+
+    private bool animationFinished = false;
     void Start(){
         bossSpriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(PlayAnimationAndWait());
     }
 
+    IEnumerator PlayAnimationAndWait(){
+        animator.Play("Taunt");
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        animationFinished = true;
+    }
     // Update is called once per frame
     void Update()
     {
-        MoveTowardsPlayerXAxis();
-
+        if(animationFinished){
+            MoveTowardsPlayerXAxis();
+        }
     }
 
     private void MoveTowardsPlayerXAxis(){
         if(target != null){
+            animator.SetFloat("speed",1);
             Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, transform.position.z);
             transform.position = new Vector3(Vector3.MoveTowards(transform.position, targetPosition,speed*Time.deltaTime).x,
             transform.position.y, transform.position.z);
+
+            if(Mathf.Approximately(target.position.x, transform.position.x)){
+            animator.SetFloat("speed", 0);
+            }
         }
-        while(transform.position.x != target.position.x){
-            isMoving = true;
-        }
-        isMoving=false;
+        
+        
     }
-    
 }
