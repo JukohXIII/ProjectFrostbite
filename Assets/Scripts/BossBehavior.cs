40b9804b6eae50cf2ed6fa66d0bcc5a45e885ccd
Jérémy Vanin <jeremyvanin@proton.me>
2024-04-18 20:40:34 +0200

    Added some primary comments

--- Assets/Scripts/BossBehavior.cs
+++ Assets/Scripts/BossBehavior.cs
@@ -12,49 +12,65 @@ public class BossBehavior : MonoBehaviour
     private bool isFacingRight = false;
     
     void Start(){
+        // Gets the sprite renderer of the boss
         bossSpriteRenderer = GetComponent<SpriteRenderer>();
         StartCoroutine(PlayAnimationAndWait());
     }
 
+    // Routine for the Taunt animator to play ONCE
     IEnumerator PlayAnimationAndWait(){
         animator.Play("Taunt");
+        //Wait for the end of the animation
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
         animationFinished = true;
     }
+
     // Update is called once per frame
     void Update()
     {
+        // If taunt animation is finished
         if(animationFinished){
             MoveTowardsPlayerXAxis();
         }
     }
 
     private void MoveTowardsPlayerXAxis(){
+        // If the target is loaded
         if(target != null){
+            // Set the animation param "speed" to 1 to start the animation
             animator.SetFloat("speed",1);
+            // Gets the target position on X axis and create a new vector
             Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, transform.position.z);
+            // Makes the boss move to the created previously created position with a new vector
             transform.position = new Vector3(Vector3.MoveTowards(transform.position, targetPosition,speed*Time.deltaTime).x,
             transform.position.y, transform.position.z);
 
+            // If the boss' position is quite the same as the player's position, sets the animation param "speed" to 0 to stop the moving animation
+            // and start the idle animation
             if(Mathf.Approximately(target.position.x, transform.position.x)){
             animator.SetFloat("speed", 0);
             }
 
+            // If the boss is on the player's right
             if(target.position.x < transform.position.x){
+                // If it is facing right
                 if(isFacingRight){
+                    // Reverses the boss' sprite orientation
                     Vector3 localScale = transform.localScale;
                     localScale.x *= -1f;
                     transform.localScale = localScale;
                     isFacingRight = false;
                 }
+            // If the boss is on the player's left
             }else if(target.position.x > transform.position.x){
+                // If the boss is not facing right
                 if(!isFacingRight){
+                    // Reverses the boss' sprite orientation
                     Vector3 localScale = transform.localScale;
                     localScale.x *= -1f;
                     transform.localScale = localScale;
                     isFacingRight = true;
-                }
-                
+                }  
             }else{} 
         }
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,7 +22,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Retrieves the raw value of the input axis named "Horizontal".
         horizontal = Input.GetAxisRaw("Horizontal");
+        // Set the animation param "speed" with the raw value on Horizontal axis
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
         CheckJumping();
         CheckKeys();
@@ -31,6 +33,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Defines the velocity of the player on X axis (raw input value) and Y axis (relative to rigidBody attributes)
         rigidBody.velocity = new Vector2(horizontal * speed, rigidBody.velocity.y);
     }
     private bool IsGrounded()
@@ -39,22 +42,29 @@ public class PlayerMovement : MonoBehaviour
     }
     private void ChangeSpriteDirection()
     {
+        // If player's sprite is looking to the right and going left or if it is not facing right and going right
         if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
+            // Change the state of the isFacingRight boolean
             isFacingRight = !isFacingRight;
+            // Flip the player's sprite
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
     }
+
+    // Makes the players goes down faster than falling
     private void goDown()
     {
+        // Only available is the player is not on ground
         if (IsGrounded() == false)
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, -10);
         }
     }
 
+    // Checks if some keys are pressed and do the action linked to the input
     private void CheckKeys(){
         if (Input.GetKeyDown(KeyCode.S))
         {
@@ -77,17 +87,20 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("HeavyAttack",false);
         }
     }
+
     private void CheckJumping(){
+        // If the people is on the ground and press the default button to jump
         if(Input.GetButtonDown("Jump") && IsGrounded()){
+            // Makes the character jump
             isJumping = true;
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpingPower);
         }
-
+        // WHAT THE HELL DID I DO HERE ? 
         if (Input.GetButtonDown("Jump") && rigidBody.velocity.y > 0f)
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y);
         }
-
+        // If the player is falling, the fall is accelerated
         if (rigidBody.velocity.y < 0f)
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, 1.001f * rigidBody.velocity.y);
